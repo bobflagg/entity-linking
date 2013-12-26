a0c7c32d90beeb07c5e8413c7d66760bb8adc3b6
Robert Flagg <rflagg@duck2.cert.qbase.us>
2013-12-26 10:31:23 -0500

    Removing obsolete files.

--- disambiguation/src/model/model_test.py
+++
@@ -1,8 +0,0 @@
-from model import build_lda_topic_model
-
-path = "/home/disambiguation/data-sets/js-test-context"
-path = "/home/disambiguation/data-sets/mycorpus.txt"
-description = "js test context corpus"
-description = "Toy corpus example from  Deerwester et al. (1990)"
-num_topics = 100
-corpus, model = build_lda_topic_model(path, description, num_topics)
