21f89eec0e1b3df47be4b1c2c8523024a97e9a0a
Robert Flagg <rflagg@duck2.cert.qbase.us>
2013-12-26 10:33:43 -0500

    Removing obsolete files.

--- disambiguation/src/feature/__init__.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-#http://scikit-learn.org/stable/modules/feature_extraction.html
-def tf_idf_features(corpus):
-  vectorizer = TfidfVectorizer(min_df=2)
-  X = vectorizer.fit_transform(corpus.documents)
-  return vectorizer
-  
