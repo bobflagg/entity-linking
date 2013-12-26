e1242b027e7476b97611c1672c4c70a57675c460
Robert Flagg <rflagg@duck2.cert.qbase.us>
2013-12-26 10:33:07 -0500

    Removing obsolete files.

--- disambiguation/src/pipeline/hierarchical_disambiguator.py
+++
@@ -1,5 +0,0 @@
-from data.corpus_builder import build_context_corpus
-
-def get_context_features(home, directory, target):
-  path = "%s/%s/%s-context" % (home, directory, target)
-  data = build_context_corpus(path)
