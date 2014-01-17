a6a974826e65914bde8a08fc63e9be3b5b48029f
Bob Flagg <bob@calcworks.net>
2014-01-17 08:58:36 -0500

    Added surface form collection.

--- disambiguation/src/kb/kb_populator.py
+++ disambiguation/src/kb/kb_populator.py
@@ -1,24 +1,34 @@
 '''
-1. activate the appropriate Python environment:
-  source /home/disambiguation/venv/bin/activate
-2. update your Python path:
+1. update your Python path:
   export PYTHONPATH=/home/disambiguation/entity-linking/disambiguation/src
-3. move to the directory containing the collector script:
+2. move to the directory containing the collector script:
   cd /home/disambiguation/entity-linking/disambiguation/src/kb
-4. run the script:
+3. run the script:
   python kb_populator.py <path to config file> [options]
 for example:
   python kb_populator.py /home/disambiguation/config/kb-population.cfg -t
 Available options are:
   
+    -k          Update keyphrase features
+    -s          Update surface form data
     -t          Update document topic model
     -v          Update context vector space model
+
+The configuration file is pretty simple and the only thing that you'll probably want
+to change is the search-pattern entry in the main section, which specifies the directories
+below
+	/home/disambiguation/corpus/entity
+to process.  Currently it is set with
+	search-pattern: js-*
+so all the John Smith directories are processed.
 
 '''
 import codecs
 import ConfigParser
 from data.corpus_builder import DirectoryBackedCorpus
+from data.domain import SUBTYPES
 from gensim import corpora, models
+import glob
 import json as simplejson
 from kpe.key_concept_extractor import extract_kc
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -75,6 +85,38 @@ def build_context_vector_space_model(home, config, update=False):
   else:
     print "Ambient context vector space model appears to be up-to-date."
 
+def collect_entity_surface_form_data(directory, document_count_map, mention_count_map):
+  '''
+  Collects and stores all surface forms appearing in feature entity kb files.
+  '''
+  path = "%s/features-entities.json" % directory
+  fp = codecs.open(path, 'r', 'UTF-8')
+  data = simplejson.load(fp)
+  for item in data:
+    subtype = item['subtype']
+    if subtype in SUBTYPES: print item
+  fp.close()
+
+def collect_surface_form_data(home, config, update=False):
+  '''
+  Collects and stores surface form for all specified (with a search pattern) entities.
+  '''
+  path = '%s/output/surface-forms.txt' % home
+  if update or not os.path.exists(path):
+    print "Collecting and storing KB surface forms."
+    start = time.time()
+    document_count_map = {}
+    mention_count_map = {}
+    # process all entities
+    search_pattern = "%s/output/entity/%s" % (home, config.get('surface-form','search-pattern'))
+    for directory in glob.glob(search_pattern):
+      collect_entity_surface_form_data(directory, document_count_map, mention_count_map)  
+      break  
+    finish = time.time()
+    print '\ttook %0.3f s' % (finish-start)
+  else:
+    print "KB surface list appears to be up-to-date."
+
 def extract_topic_features(output_directory, documents , dictionary, model, tolerance):
   '''
   Extracts and stores topic features from the given documents, using the
@@ -106,9 +148,9 @@ def extract_entity_features(output_directory, f='Proximity_AllCollapsedContext_J
   surface_form_pattern = "|".join(surface_forms)
   surface_form_re = re.compile(surface_form_pattern, re.IGNORECASE)
   mentions_path = '%s/%s' % (output_directory, f)
-  print '-->>', mentions_path
   mentions_data = codecs.open(mentions_path, 'r', 'UTF-8').readlines()
   features = []
+  finished_doc = True
   for line in mentions_data: 
     line = line.strip()
     if line:
@@ -164,11 +206,10 @@ class SimpleCorpus(object):
       tokens = sum(tokenized_sentences, [])
       yield self.dictionary.doc2bow(tokens)
 
-def extract_features(home, config, opts):
-  print "Extracting features."
+def extract_entity_kb_data(home, config, opts, doc_dictionary, doc_model, entity):
+  print " ->> Extracting features for entity %s." % entity
   kb_entry = {}
   output_directory = "%s/output" % home
-  entity = config.get('main','entity')
   target_directory = "%s/entity/%s" % (output_directory, entity)
   if not os.path.exists(target_directory): os.makedirs(target_directory)
   # get entity documents
@@ -178,9 +219,7 @@ def extract_features(home, config, opts):
     documents.append(codecs.open("%s/%s" % (path, d), 'r', 'UTF-8').read())
   # topic features
   tolerance = config.getfloat('document-topic-model','feature-tolerance')
-  dictionary = corpora.Dictionary.load('%s/ambient-document.dict' % output_directory)
-  model = models.ldamodel.LdaModel.load('%s/ambient-document.lda' % output_directory)
-  extract_topic_features(target_directory, documents , dictionary, model, tolerance)
+  extract_topic_features(target_directory, documents , doc_dictionary, doc_model, tolerance)
   # keyphrase features
   max_keyphrases= config.getint('keyphrase','max-keyphrases')
   extract_keyphrase_features(target_directory, documents, max_keyphrases, opts.update_keyphrase_features)
@@ -188,11 +227,24 @@ def extract_features(home, config, opts):
   #extract_entity_features(target_directory, f='Proximity_AllCollapsedContext_JS', surface_forms = ['Smith', '^John$'])
   extract_entity_features(target_directory)
 
+def extract_kb_data(home, config, opts):
+  print "Extracting features."
+  output_directory = "%s/output" % home
+  doc_dictionary = corpora.Dictionary.load('%s/ambient-document.dict' % output_directory)
+  doc_model = models.ldamodel.LdaModel.load('%s/ambient-document.lda' % output_directory)
+  # process all entities
+  search_pattern = "%s/corpus/entity/%s" % (home, config.get('main','search-pattern'))
+  for d in glob.glob(search_pattern):
+    extract_entity_kb_data(home, config, opts, doc_dictionary, doc_model, d.split('/')[-1])
+
 if __name__ == "__main__":
   # Parse command-line options.
   parser = optparse.OptionParser()
+
+  parser.add_option("-b", help="Skip extracting KB data", dest='skip_extracting_kb_data', default=False, action='store_true')
+  parser.add_option("-k", help="Update keyphrase features", dest='update_keyphrase_features', default=False, action='store_true')
+  parser.add_option("-s", help="Update surface form data", dest='update_surface_form_data', default=False, action='store_true')
   parser.add_option("-t", help="Update document topic model", dest='update_topic_model', default=False, action='store_true')
-  parser.add_option("-k", help="Update keyphrase featyres", dest='update_keyphrase_features', default=False, action='store_true')
   parser.add_option("-v", help="Update context vector space model", dest='update_vector_model', default=False, action='store_true')
   (opts, args) = parser.parse_args()
   # Read configuration info:
@@ -206,4 +258,6 @@ if __name__ == "__main__":
   # Build ambient context corpus vector space model
   build_context_vector_space_model(home, config, update=opts.update_vector_model)
   # Extract features
-  extract_features(home, config, opts)
+  if not opts.skip_extracting_kb_data: extract_kb_data(home, config, opts)
+  # Collect surface forms
+  collect_surface_form_data(home, config, update=opts.update_surface_form_data)
